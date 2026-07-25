4fa885fa962bca05e0ac318f6557fd18d3fa4b83
ASUR PAPA <jattlilu6580@gmail.com>
2026-07-25 12:14:58 +0530

    Update play.py

--- AloneX/plugins/play.py
+++ AloneX/plugins/play.py
@@ -121,7 +121,15 @@ async def play_hndlr(
             await sent.edit_text(m.lang["play_downloading"])
             file.file_path = await yt.download(file.id, video=video)
 
-    await anon.play_media(chat_id=m.chat.id, message=sent, media=file)
+
+await anon.play_media(chat_id=m.chat.id, message=sent, media=file)
+    
+    # ✅ DELETE USER'S COMMAND MESSAGE
+    try:
+        await m.delete()
+    except:
+        pass
+    
     if not tracks:
         return
     added = playlist_to_queue(m.chat.id, tracks)
